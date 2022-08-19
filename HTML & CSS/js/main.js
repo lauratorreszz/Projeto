fa719daae7ba1772db12db3926763566f77371c9
lauratorreszz <105013308+lauratorreszz@users.noreply.github.com>
2022-08-19 16:44:04 -0300

    Add files via upload

---
+++ HTML & CSS/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+    //fixed header
+    window.onscroll = function(){
+        if(window.pageYOffset > 140) {
+            $('#barra_nav').addClass("active");
+        }else{
+            $('#barra_nav').addClass("active");
+        }
+    }
+});
+
+//noticias
